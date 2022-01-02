25f18df2f3a977569607c469415f2f32821b5732
Piyush <piyushguptaa2z123@gmail.com>
2022-01-02 14:20:41 +0530

    desktop version is ready

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,22 +7,46 @@ const ProjectCard = ({ project }) => {
     <div className="flex project-card shadow-lg rounded-xl overflow-hidden">
       <div className="group object-cover flex-[5] overflow-hidden relative">
         <img
-          className=" hover:scale-110 transition-all duration-300 border-2 border-red-100 w-full h-full group-hover:opacity-30 block"
+          className={`group-hover:scale-110 transition-all duration-300 border-2 border-red-100 w-full h-full ${
+            links["Live"] && "group-hover:opacity-30"
+          } block`}
           src={
             require(`../images/${title.split(" ").join("-").toLowerCase()}.png`)
               .default
           }
           alt={title}
         />
-        <div
-          className={`middle absolute top-2/4 left-2/4 opacity-0 group-hover:opacity-100 -translate-x-2/4 -translate-y-2/4 transition-all duration-300 `}
-        >
-          <a href={links["Live"]}>ICON</a>
-        </div>
+        {links["Live"] && (
+          <div
+            className={`middle absolute top-2/4 left-2/4 opacity-0 group-hover:opacity-100 -translate-x-2/4 -translate-y-2/4 transition-all duration-300 `}
+          >
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={links["Live"]}
+              className="py-2 px-4 font-medium bg-red-100 rounded-md"
+            >
+              view
+            </a>
+          </div>
+        )}
       </div>
       <div className="px-5 py-4 flex-[7]">
-        <div>
-          <span className="text-2xl block font-semibold mb-2">{title}</span>
+        <div className=" flex justify-between items-center">
+          <div>
+            <span className="text-2xl block font-semibold mb-2">{title}</span>
+          </div>
+          <div className="flex gap-2 items-center">
+            {Object.entries(links).map(([key, value], idx) => (
+              <a
+                key={idx}
+                href={value}
+                className="py-1 px-4 border-2 font-bold border-gray-600  hover:bg-red-100 rounded-md"
+              >
+                {key}
+              </a>
+            ))}
+          </div>
         </div>
         <div className="mb-3">
           <span>{description}</span>
